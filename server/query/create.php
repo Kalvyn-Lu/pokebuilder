a76f9256d636dc3e7fc2258d5bf36c66d53677e8
enderdelphiki <ender.delphiki@yahoo.com>
2014-12-09 13:27:17 -0700

    added season 7 rules

--- server/query/create.php
+++ server/query/create.php
@@ -3,48 +3,20 @@
 
 require_once("getquery.php");
 
-$item = 2027;
+$handle = @fopen("regulations.txt", "r");
 
-run_query("INSERT INTO banitem (ruleset, item) VALUES
-(4,$item),
-(5,$item),
-(6,$item),
-(7,$item),
-(10,$item),
-(11,$item),
-(12,$item),
-(13,$item)
-");
-
-die("Done.");
-
-$mons= json_decode(to_json(run_query("SELECT * from species WHERE type1=3 || type2=3")));
-
-$ids = array();
-$i = 0;
-foreach ($mons as $key=>$value) {
-    $ids[$i++] = $mons[$key]->id;
-}
-
-foreach($ids as $key=>$value) {
-    $moves = json_decode(to_json(run_query("SELECT * FROM moveset WHERE id=$value;")))[0];
-    
-    $movelist = explode(" ", $moves->moves);
-    
-    foreach($movelist as $key2=>$value2) {
-        if (446 == $value2) {
-            echo "$value gets it!<br>";
-        }
-    }
-    $movelist = explode(" ", $moves->egg_moves);
-    
-    foreach($movelist as $key2=>$value2) {
-        if (446 == $value2) {
-            echo "$value gets it!<br>";
-        }
+$query = "INSERT INTO banspecies (ruleset, species) VALUES ";
+if ($handle) {
+    while (($buffer = fgets($handle)) !== false) {
+        $buffer = substr($buffer, 0, strlen($buffer) - 2);
+        $ids = json_decode(to_json(run_query("
+            SELECT id FROM species WHERE name=\"$buffer\"
+        ")));
+        $query.="(1, ".$ids[0]->id."),";
     }
 }
-
+echo $query;
+run_query(substr($query, 0, strlen($query) - 1));
 die("Done!");
 
 ?>
--- server/query/searchmoves.php
+++ server/query/searchmoves.php
@@ -1,5 +1,36 @@
 <?php
 
+/*
+$item = 2027;
+
+$mons= json_decode(to_json(run_query("SELECT * from species WHERE type1=3 || type2=3")));
+
+$ids = array();
+$i = 0;
+foreach ($mons as $key=>$value) {
+    $ids[$i++] = $mons[$key]->id;
+}
+
+foreach($ids as $key=>$value) {
+    $moves = json_decode(to_json(run_query("SELECT * FROM moveset WHERE id=$value;")))[0];
+    
+    $movelist = explode(" ", $moves->moves);
+    
+    foreach($movelist as $key2=>$value2) {
+        if (446 == $value2) {
+            echo "$value gets it!<br>";
+        }
+    }
+    $movelist = explode(" ", $moves->egg_moves);
+    
+    foreach($movelist as $key2=>$value2) {
+        if (446 == $value2) {
+            echo "$value gets it!<br>";
+        }
+    }
+}
+
+*/
 require_once("getquery.php");
 
 $s = get("species");
